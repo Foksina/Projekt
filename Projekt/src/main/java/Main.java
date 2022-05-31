ba9a6d10e0d5bb39b4cff43832a3dc1f4ef678b5
Foksina <106610411+Foksina@users.noreply.github.com>
2022-05-31 17:14:39 +0200

    Update Main.java

--- Projekt/src/main/java/Main.java
+++ Projekt/src/main/java/Main.java
@@ -1,5 +1,23 @@
 public class Main {
-    public void main((String[] args){
+    private int liczbaPszczol = 4;
+    private int liczbaSzerszeni = 2;
 
+    public int getLiczbaPszczol(){
+        return liczbaPszczol;
+    }
+    public void setLiczbaPszczol(int liczbaPszczol){
+        this.liczbaPszczol = liczbaPszczol;
+    }
+
+    public int getLiczbaSzerszeni(){
+        return liczbaSzerszeni;
+    }
+    public void setLiczbaSzerszeni(int liczbaSzerszeni){
+        this.liczbaSzerszeni = liczbaSzerszeni;
+    }
+
+    public static int main(String[] args) {
+        new Łąka();
+        return 0;
     }
 }
