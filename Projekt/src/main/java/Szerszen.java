3f136318fe00ede83b21539c452f1ebde8e356fd
Foksina <106610411+Foksina@users.noreply.github.com>
2022-06-12 17:36:21 +0200

    Update Szerszen.java

--- Projekt/src/main/java/Szerszen.java
+++ Projekt/src/main/java/Szerszen.java
@@ -1,5 +1,20 @@
 public class Szerszen extends Owad{
-    private Szerszen(){
-        this.szybkoscPoruszania = 1;
+
+    public Szerszen(){
+        this.szybkoscPoruszania = 2;
+    }
+    public void walka(){
+        if(pszczolWokol == 1){
+            (Laka.spisPszczol).remove(listaPszczolWokol.get(0));
+            Laka.planszaOwadow[listaPszczolWokol.get(0).getWspolrzedne()[0]][listaPszczolWokol.get(0).getWspolrzedne()[1]] = null;
+
+            (Laka.object2).setLiczbaPszczol(((Laka.object2).getLiczbaPszczol()-1));
+        }
+        else if (pszczolWokol>1){
+            (Laka.spisSzerszeni).remove(Laka.planszaOwadow[getWspolrzedne()[0]][getWspolrzedne()[1]]);
+            Laka.planszaOwadow[getWspolrzedne()[0]][getWspolrzedne()[1]] = null;
+
+            (Laka.object2).setLiczbaSzerszeni(((Laka.object2).getLiczbaSzerszeni()-1));
+        }
     }
 }
