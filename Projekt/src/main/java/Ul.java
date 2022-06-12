1957ba61302a5f382cd82c3bb990b563c19de38a
Foksina <106610411+Foksina@users.noreply.github.com>
2022-06-12 17:36:51 +0200

    Update Ul.java

--- Projekt/src/main/java/Ul.java
+++ Projekt/src/main/java/Ul.java
@@ -1,15 +1,42 @@
+import java.util.Random;
+
 public class Ul {
     private int sumaNektaru = 0;
-    public int iloscPszczol;
 
     private void liczNektar(int zebranyNektar){
-        sumaNektaru += zebranyNektar;
         if(sumaNektaru >= 50){
             tworzeniePszczoly();
             sumaNektaru -= 50;
         }
     }
-    private void tworzeniePszczoly(){
 
+    public int getSumaNektaru() {
+        return sumaNektaru;
+    }
+    public void setSumaNektaru(int nowyNektar) {
+        sumaNektaru += nowyNektar;
+    }
+    private void tworzeniePszczoly(){
+        Pszczola object = new Pszczola();
+        (Laka.spisPszczol).add(object);
+
+        int x,y;
+        boolean n = true;
+        while(n == true){
+            Random random = new Random();
+            x = random.nextInt(Laka.getRozmiarLaki());
+            Random random2 = new Random();
+            y = random2.nextInt(Laka.getRozmiarLaki());
+            if((Laka.planszaOwadow[x][y] == null) && (x != (int)Laka.a) && (y != (int)Laka.b)){
+                Laka.planszaOwadow[x][y] = object;
+                int [] pomocniczna = new int[2];
+                pomocniczna[0]=x;
+                pomocniczna[1]=y;
+                object.setWspolrzedne(pomocniczna);
+                break;
+            }
+        }
+
+        (Laka.object2).setLiczbaPszczol(((Laka.object2).getLiczbaPszczol()+1));
     }
 }
