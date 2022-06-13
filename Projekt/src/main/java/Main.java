637e0c04e55b11641690a28354eb2791a2e7d5a3
Foksina <106610411+Foksina@users.noreply.github.com>
2022-06-14 00:07:04 +0200

    Update Main.java

--- Projekt/src/main/java/Main.java
+++ Projekt/src/main/java/Main.java
@@ -1,71 +1,31 @@
-import java.util.Scanner;
 public class Main {
-    private static int liczbaPszczol;
-    private static int liczbaSzerszeni;
-    public int getLiczbaPszczol(){
-        return liczbaPszczol;
-    }
-    public void setLiczbaPszczol(int liczbaPszczol){
-        this.liczbaPszczol = liczbaPszczol;
-    }
-
-    public int getLiczbaSzerszeni(){
-        return liczbaSzerszeni;
-    }
-    public void setLiczbaSzerszeni(int liczbaSzerszeni){
-        this.liczbaSzerszeni = liczbaSzerszeni;
-    }
-
     public static void main(String[] args) {
-        Laka object = new Laka();
+        Dane object2 = new Dane();
+        object2.sprawdzDane();
 
-        while(true){
-            System.out.println("Podaj liczbę pszczół: ");
-            Scanner odczyt = new Scanner(System.in);
-            liczbaPszczol = odczyt.nextInt();
-            if(liczbaPszczol <0){
-                System.out.println("Liczba pszczół nie może być ujemna!");
-            }
-            else if (liczbaPszczol > (0.5*(object.getRozmiarLaki()*object.getRozmiarLaki()))) {
-                System.out.println("Liczba pszczół jest zbyt duża!\nPszczół może być maksymalnie "+(int)(0.5*(object.getRozmiarLaki()*object.getRozmiarLaki())));
-            }
-            else {
-                break;
-            }
-        }
+        int liczbaPszczol = object2.getLiczbaPszczol();
+        int liczbaSzerszeni = object2.getLiczbaSzerszeni();
 
-        while(true){
-            System.out.println("Podaj liczbę szerszeni: ");
-            Scanner odczyt2 = new Scanner(System.in);
-            liczbaSzerszeni = odczyt2.nextInt();
-            if(liczbaSzerszeni <0){
-                System.out.println("Liczba szerszeni nie może być ujemna!");
-            }
-            else if (liczbaSzerszeni > ((object.getRozmiarLaki()*object.getRozmiarLaki())- liczbaPszczol-1)) {
-                System.out.println("Liczba szerszeni jest zbyt duża!\nSzerszeni może być maksymalnie "+(int)((object.getRozmiarLaki()*object.getRozmiarLaki())-liczbaPszczol-1));
-            }
-            else {
-                break;
-            }
-        }
-
-        object.ustawieniaPoczatkowe();
+        (object2.object).ustawieniaPoczatkowe();
         while(liczbaPszczol>0 && liczbaSzerszeni>0){
 
-            for(int i=0; i<(object.spisPszczol).size();i++){
-                ((object.spisPszczol).get(i)).kolejkaPszczoly();
+            for(int i=0; i<(object2.object.spisPszczol).size();i++){
+                ((object2.object.spisPszczol).get(i)).kolejkaPszczoly();
             }
 
-            for(int j=0; j<(object.spisSzerszeni).size(); j++){
-                ((object.spisSzerszeni).get(j)).kolejkaSzerszenia();
+            for(int j=0; j<(object2.object.spisSzerszeni).size(); j++){
+                ((object2.object.spisSzerszeni).get(j)).kolejkaSzerszenia();
             }
+
+            liczbaPszczol = object2.getLiczbaPszczol();
+            liczbaSzerszeni = object2.getLiczbaSzerszeni();
         }
 
         if(liczbaPszczol==0){
-            System.out.println("Liczba pszczół wynosi 0. Szerszenie wygrały!");
+            System.out.println("Liczba pszczół wynosi 0. Szerszenie wygrały!\nLiczba szerszeni wynosi "+ liczbaSzerszeni);
         }
         else if(liczbaSzerszeni==0){
-            System.out.println("Liczba szerszeni wynosi 0. Pszczoły wygrały!");
+            System.out.println("Liczba szerszeni wynosi 0. Pszczoły wygrały!\nLiczba pszczół wynosi "+ liczbaPszczol);
         }
         else{
             System.out.println("Symulacja zakończyła się niepowodzeniem!");
