8419484437280a65716fbb0c52b23bc7e6f0f735
Foksina <106610411+Foksina@users.noreply.github.com>
2022-06-14 00:09:43 +0200

    Update Szerszen.java

--- Projekt/src/main/java/Szerszen.java
+++ Projekt/src/main/java/Szerszen.java
@@ -11,16 +11,20 @@ public class Szerszen extends Owad{
     }
     public void walka(){
         if(pszczolWokol == 1){
-            (Laka.spisPszczol).remove(listaPszczolWokol.get(0));
+            Owad pszcz = listaPszczolWokol.get(0);
+            (Laka.spisPszczol).remove(pszcz);
             Laka.planszaOwadow[listaPszczolWokol.get(0).getWspolrzedne()[0]][listaPszczolWokol.get(0).getWspolrzedne()[1]] = null;
 
             (Laka.object2).setLiczbaPszczol(((Laka.object2).getLiczbaPszczol()-1));
+            System.out.println("Zabito pszczołę!");
         }
         else if (pszczolWokol>1){
-            (Laka.spisSzerszeni).remove(Laka.planszaOwadow[getWspolrzedne()[0]][getWspolrzedne()[1]]);
+            Owad szer = Laka.planszaOwadow[getWspolrzedne()[0]][getWspolrzedne()[1]];
+            (Laka.spisSzerszeni).remove(szer);
             Laka.planszaOwadow[getWspolrzedne()[0]][getWspolrzedne()[1]] = null;
 
             (Laka.object2).setLiczbaSzerszeni(((Laka.object2).getLiczbaSzerszeni()-1));
+            System.out.println("Zabito szerszenia!");
         }
     }
 }
