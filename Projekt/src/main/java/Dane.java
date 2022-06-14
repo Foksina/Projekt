22f611b940046a9e88969502e0facfd5c14f94dc
Foksina <106610411+Foksina@users.noreply.github.com>
2022-06-14 12:44:51 +0200

    Update Dane.java

--- Projekt/src/main/java/Dane.java
+++ Projekt/src/main/java/Dane.java
@@ -20,14 +20,14 @@ public class Dane {
 
     public void sprawdzDane(){
         while(true){
-            System.out.println("Podaj liczbę pszczół: ");
+            System.out.println("Podaj liczbe pszczol: ");
             Scanner odczyt = new Scanner(System.in);
             liczbaPszczol = odczyt.nextInt();
             if(liczbaPszczol <0){
-                System.out.println("Liczba pszczół nie może być ujemna!");
+                System.out.println("Liczba pszczol nie moze byc ujemna!");
             }
             else if (liczbaPszczol > (0.8*(object.getRozmiarLaki()*object.getRozmiarLaki()))) {
-                System.out.println("Liczba pszczół jest zbyt duża!\nPszczół może być maksymalnie "+(int)(0.8*(object.getRozmiarLaki()*object.getRozmiarLaki())));
+                System.out.println("Liczba pszczol jest zbyt duza!\nPszczol moze byc maksymalnie "+(int)(0.8*(object.getRozmiarLaki()*object.getRozmiarLaki())));
             }
             else {
                 break;
@@ -35,14 +35,14 @@ public class Dane {
         }
 
         while(true){
-            System.out.println("Podaj liczbę szerszeni: ");
+            System.out.println("Podaj liczbe szerszeni: ");
             Scanner odczyt2 = new Scanner(System.in);
             liczbaSzerszeni = odczyt2.nextInt();
             if(liczbaSzerszeni <0){
-                System.out.println("Liczba szerszeni nie może być ujemna!");
+                System.out.println("Liczba szerszeni nie moze byc ujemna!");
             }
             else if (liczbaSzerszeni > ((object.getRozmiarLaki()*object.getRozmiarLaki())- liczbaPszczol-1)) {
-                System.out.println("Liczba szerszeni jest zbyt duża!\nSzerszeni może być maksymalnie "+(int)((object.getRozmiarLaki()*object.getRozmiarLaki())-liczbaPszczol-1));
+                System.out.println("Liczba szerszeni jest zbyt duza!\nSzerszeni moze byc maksymalnie "+(int)((object.getRozmiarLaki()*object.getRozmiarLaki())-liczbaPszczol-1));
             }
             else {
                 break;
