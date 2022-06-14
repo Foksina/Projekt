d3a40690d3d7bcc07525f1d53bf6c92ce43ed227
Foksina <106610411+Foksina@users.noreply.github.com>
2022-06-14 12:01:27 +0200

    Update Main.java

--- Projekt/src/main/java/Main.java
+++ Projekt/src/main/java/Main.java
@@ -1,25 +1,51 @@
 public class Main {
+    public static int liczbaPszczol;
+    public static int liczbaSzerszeni;
+    public static Plansza plansza;
+
+    public static void wait(int ms)
+    {
+        try
+        {
+            Thread.sleep(ms);
+        }
+        catch(InterruptedException ex)
+        {
+            Thread.currentThread().interrupt();
+        }
+    }
+
     public static void main(String[] args) {
         Dane object2 = new Dane();
         object2.sprawdzDane();
 
-        int liczbaPszczol = object2.getLiczbaPszczol();
-        int liczbaSzerszeni = object2.getLiczbaSzerszeni();
+        liczbaPszczol = object2.getLiczbaPszczol();
+        liczbaSzerszeni = object2.getLiczbaSzerszeni();
+
+        plansza = new Plansza();
+        plansza.setVisible(true);
 
         (object2.object).ustawieniaPoczatkowe();
         while(liczbaPszczol>0 && liczbaSzerszeni>0){
 
             for(int i=0; i<(object2.object.spisPszczol).size();i++){
                 ((object2.object.spisPszczol).get(i)).kolejkaPszczoly();
+                liczbaPszczol = object2.getLiczbaPszczol();
+                liczbaSzerszeni = object2.getLiczbaSzerszeni();
+                plansza.repaint();
+                wait(1000);
             }
 
             for(int j=0; j<(object2.object.spisSzerszeni).size(); j++){
                 ((object2.object.spisSzerszeni).get(j)).kolejkaSzerszenia();
+                liczbaPszczol = object2.getLiczbaPszczol();
+                liczbaSzerszeni = object2.getLiczbaSzerszeni();
+                plansza.repaint();
+                //wait(1000);
             }
-
-            liczbaPszczol = object2.getLiczbaPszczol();
-            liczbaSzerszeni = object2.getLiczbaSzerszeni();
         }
+
+        plansza.repaint();
 
         if(liczbaPszczol==0){
             System.out.println("Liczba pszczół wynosi 0. Szerszenie wygrały!\nLiczba szerszeni wynosi "+ liczbaSzerszeni);
