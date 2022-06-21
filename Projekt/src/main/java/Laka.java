533db603d60aa13e2a93af0c73b65d7db44e318e
Foksina <106610411+Foksina@users.noreply.github.com>
2022-06-21 21:49:12 +0200

    Update Laka.java

--- Projekt/src/main/java/Laka.java
+++ Projekt/src/main/java/Laka.java
@@ -1,31 +1,69 @@
 import java.util.Random;
 import java.util.LinkedList;
 import java.util.List;
+/**
+ *Klasa odpowiedzialna za tworzenie planszy (łąki) symulacji, obiektów znajdujących się na planszy oraz odpowiednie ich rozmieszczenie.
+ */
 public class Laka {
+    /**
+     *zmienna określająca długość boku kwadratowej planszy
+     */
     private static int rozmiarLaki = 5;
+    /**
+     *lista obiektów typu pszczoła przechowująca wszystkie istniejąca w danym momencie symulacji obiekty tego typu
+     */
     public static List<Pszczola> spisPszczol = new LinkedList();
+    /**
+     *lista obiektów typu szerszeń przechowująca wszystkie istniejąca w danym momencie symulacji obiekty tego typu
+     */
     public static List<Szerszen> spisSzerszeni = new LinkedList();
+    /**
+     *tablica określająca położenie obiektów typu kwiat
+     */
     public static Kwiat planszaKwiatow[][];
+    /**
+     *tablica określająca położenie obiektów typu owad
+     */
     public static Owad planszaOwadow[][];
+    /**
+     *tablica określająca położenie obiektów typu ul
+     */
     public static Ul planszaUl[][];
-    public static double a;
-    public static double b; // wspólrzędne Ula w tablicy planszaUl
+    /**
+     *zmienne określająca współrzędne Ula w tablicy
+     */
+    public static double a,b;
+    /**
+     *obiekt typu Ul odpowiedzialny za gromadzenie nektaru oraz tworzenie nowych obiektów typu pszczoła
+     */
     public static Ul object = new Ul();
     public static Dane object2 = new Dane();
     public static Main object3 = new Main();
+    /**
+     *metoda pozwalająca na pobranie długości boku kwadratowej planszy
+     */
     public static int getRozmiarLaki(){
         return rozmiarLaki;
     }
+    /**
+     *metoda pozwalająca na ustawienie długości boku kwadratowej planszy
+     */
     public void setRozmiarLaki(int rozmiarLaki){
         this.rozmiarLaki = rozmiarLaki;
     }
 
+    /**
+     *metoda odpowiedzialna za wywołania metod tworzenia planszy, tworzenia obiektów znajdujących się na planszy oraz ich rozlokowania dla paramterów początkowych symulacji
+     */
     public void ustawieniaPoczatkowe(){
         tworzenieLaki();
         ustawUl();
         wypelnijLake();
         ustawOwady();
     }
+    /**
+     *metoda zadająca rozmiary tablicom odpowiednich obiektów oraz wypełniająca wszystkie ich pola wartościom null
+     */
     private static void tworzenieLaki(){  //nadanie rozmiaru wszystkim tablicom oraz wypelnienie obiektami typu null
         planszaKwiatow = new Kwiat[rozmiarLaki][rozmiarLaki];
         planszaOwadow = new Owad[rozmiarLaki][rozmiarLaki];
@@ -38,6 +76,9 @@ public class Laka {
             }
     }
 
+    /**
+     *metoda obliczająca współrzędne ula (środek planszy) oraz ustawiająca ul w odpowiednim miejscu
+     */
     private void ustawUl() {
         if(rozmiarLaki%2 ==0){
             a = ((double)rozmiarLaki/2) - 1;
@@ -50,6 +91,9 @@ public class Laka {
         planszaUl[(int)a][(int)b]=object;
     }
 
+    /**
+     *metoda tworząca obiekty typu kwiat, losująca ich położenie oraz umieszczająca obiekty w odpowiednich miejscach tablicy
+     */
     private void wypelnijLake() {
         int x,y; //zmienne do losowania wspolrzednych kwiatow
        //dodawanie lawnedy do tablicy planszaKwiatow
@@ -88,7 +132,9 @@ public class Laka {
                 i=i-1;}
         }
     }
-
+    /**
+     *metoda tworząca obiekty typu owad, wpisująca je na odpowiednią listę obiektów, losująca ich współrzędne w tablicy oraz ustawiająca je w odpowiednim miejscu
+     */
     private void ustawOwady() {
         //stworzenie listy obiektów typu pszczola
         int n = object2.getLiczbaPszczol();
