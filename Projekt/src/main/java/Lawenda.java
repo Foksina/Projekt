cf22e9ebce6abbafc37c80620a79933a707c473a
Foksina <106610411+Foksina@users.noreply.github.com>
2022-06-21 21:49:40 +0200

    Update Lawenda.java

--- Projekt/src/main/java/Lawenda.java
+++ Projekt/src/main/java/Lawenda.java
@@ -1,5 +1,11 @@
+/**
+ *Klasa dziedzicząca po klasie Kwiat, odpowiedzialna za wskazanie wewnętrznego parametru charakterystycznego dla lawendy.
+ */
 public class Lawenda extends Kwiat{
-    private Lawenda(){
+    /**
+     *metoda odpowidzialna za nadanie odpowiedniej wartości do parametru iloscNektaru dziedziczonego z klasy Kwiat
+     */
+    public Lawenda(){
         this.iloscNektaru = 3;
     }
 }
