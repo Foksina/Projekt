27417e59206a14c2986b5ad4e0abcb8633d464d5
Foksina <106610411+Foksina@users.noreply.github.com>
2022-06-21 21:46:59 +0200

    Update Dane.java

--- Projekt/src/main/java/Dane.java
+++ Projekt/src/main/java/Dane.java
@@ -1,30 +1,55 @@
 import java.util.Scanner;
-
+/**
+ * Klasa odpowiada za pobranie danych od użytkownika i sprawdzenie ich poprawności.
+ */
 public class Dane {
+    /**
+     * zmienna określająca liczbę pszczół zadaną przez użytkownika
+     */
     private static int liczbaPszczol;
+    /**
+     * zmienna określająca liczbę szerszeni zadaną przez użytkownika
+     */
     private static int liczbaSzerszeni;
+    /**
+     * obiekt odpowiedzialny za rozmieszczenie pszczół i szerszeni na planszy
+     */
     public Laka object = new Laka();
+    /**
+     *metoda do pobrania liczby pszczół zadanej przez użytkownika
+     */
     public int getLiczbaPszczol(){
         return liczbaPszczol;
     }
+    /**
+     *metoda do ustawienia liczby pszczół
+     */
     public void setLiczbaPszczol(int liczbaPszczol){
         this.liczbaPszczol = liczbaPszczol;
     }
-
+    /**
+     *metoda do pobrania liczby szerszeni zadanej przez użytkownika
+     */
     public int getLiczbaSzerszeni(){
         return liczbaSzerszeni;
     }
+    /**
+     *metoda do ustawienia liczby szerszeni
+     */
     public void setLiczbaSzerszeni(int liczbaSzerszeni){
         this.liczbaSzerszeni = liczbaSzerszeni;
     }
 
+    /**
+     *metoda odpowiedzialna za pobrania danych od użytkownika oraz sprawdzenia ich poprawności (zgodności z założeniami symulacji)
+     */
     public void sprawdzDane(){
         while(true){
             System.out.println("Podaj liczbe pszczol: ");
             Scanner odczyt = new Scanner(System.in);
             liczbaPszczol = odczyt.nextInt();
             if(liczbaPszczol <0){
-                System.out.println("Liczba pszczol nie moze byc ujemna!");
+                System.out.println("Liczba pszczol nie moze być ujemna!");
             }
             else if (liczbaPszczol > (0.8*(object.getRozmiarLaki()*object.getRozmiarLaki()))) {
                 System.out.println("Liczba pszczol jest zbyt duza!\nPszczol moze byc maksymalnie "+(int)(0.8*(object.getRozmiarLaki()*object.getRozmiarLaki())));
