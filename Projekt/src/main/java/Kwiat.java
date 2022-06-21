f7513c89c5b7e165cd833d23e52b553eec6bed62
Foksina <106610411+Foksina@users.noreply.github.com>
2022-06-21 21:48:15 +0200

    Update Kwiat.java

--- Projekt/src/main/java/Kwiat.java
+++ Projekt/src/main/java/Kwiat.java
@@ -1,20 +1,38 @@
+/**
+ *Klasa potrzebna do tworzenia obiektów, po której dziedziczy Lawenda oraz Krokus. Zawiera współrzędne obiektów oraz zmienną odpowiedzialną za ilość nektaru w obiekcie.
+ */
 public class Kwiat {
+    /**
+     *zmienna współrzędnych obiektu
+     */
     private int [] wspolrzedne;
+    /**
+     *zmienna określająca ilość nektaru w obiekcie w danym momencie symulacji
+     */
     protected byte iloscNektaru;
 
-
+    /**
+     *metoda pozwalająca na pobranie współrzędnych obiektu
+     */
     public int[] getWspolrzedne() {
         return wspolrzedne;
     }
 
+    /**
+     *metoda pozwalająca na nadanie współrzednych obiektu
+     */
     public void setWspolrzedne(int[] wspolrzedne) {
         this.wspolrzedne = wspolrzedne;
     }
-
+    /**
+     *metoda pozwalająca na pobranie ilosci nektaru
+     */
     public byte getIloscNektaru() {
         return iloscNektaru;
     }
-
+    /**
+     *metoda pozwalająca na nadanie ilosci nektaru
+     */
     public void setIloscNektaru(byte iloscNektaru) {
         this.iloscNektaru = iloscNektaru;
     }
