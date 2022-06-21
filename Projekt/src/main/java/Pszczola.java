2b7721f5e2b47eb05c44feca0c63db969d7dfdd4
Foksina <106610411+Foksina@users.noreply.github.com>
2022-06-21 21:52:23 +0200

    Update Pszczola.java

--- Projekt/src/main/java/Pszczola.java
+++ Projekt/src/main/java/Pszczola.java
@@ -1,10 +1,22 @@
+/**
+ *Klasa dziedzicząca po klasie Owad. Odpowiedzialna za ustawienie szybkości pszczoły, określająca kolejność ruchów, symulację walki oraz zbierania nektaru i jego transportu do ula.
+ */
 public class Pszczola extends Owad{
+    /**
+     *zmienna przechowująca ilość nektaru zebranego z kwiata
+     */
     private int zebranyNektar;
 
+    /**
+     *metoda ustawiająca szybkosc poruszania pszczoly
+     */
     public Pszczola(){
         this.szybkoscPoruszania = 1;
     }
 
+    /**
+     *metoda definująca kolejność zachowań pszczoły w jej ruchu
+     */
     public void kolejkaPszczoly(){
         boolean p = sprawdzKwiat();
         if(p==true){
@@ -21,7 +33,9 @@ public class Pszczola extends Owad{
             walka();
         }
     }
-
+    /**
+     *metoda pozwalająca sprawdzić, czy na polu na którym jest pszczoła jest kwiat mający nektar
+     */
     private boolean sprawdzKwiat() {
         if(Laka.planszaKwiatow[getWspolrzedne()[0]][getWspolrzedne()[1]] != null){
             setZebranyNektar((Laka.planszaKwiatow[getWspolrzedne()[0]][getWspolrzedne()[1]]).getIloscNektaru());
@@ -32,15 +46,21 @@ public class Pszczola extends Owad{
             return false;
         }
     }
-
+    /**
+     *metoda pozwalająca na pobranie ilosci zebranego nektaru
+     */
     public int getZebranyNektar() {
         return zebranyNektar;
     }
-
+    /**
+     *metoda pozwalająca na nadanie ilosci zebranego nektaru
+     */
     public void setZebranyNektar(int nektar) {
         this.zebranyNektar += nektar;
     }
-
+    /**
+     *metoda definiująca warunki otoczenia do odbycia się walki oraz wywołująca walkę
+     */
     private void walka(){
         if(szerszeniWokol >0){
             int x = (Laka.spisSzerszeni).indexOf(listaSzerszeniWokol.get(0));
@@ -48,6 +68,9 @@ public class Pszczola extends Owad{
         }
     }
 
+    /**
+     *metoda okreslająca ruch pszczoły do ula, zostawienie tam nektaru oraz powrót na wcześniejszą pozycję
+     */
     private void nektarDoUla(){
         if(zebranyNektar>0){
             int[] x;
@@ -66,4 +89,5 @@ public class Pszczola extends Owad{
             Laka.planszaOwadow[x[0]][x[1]] = this;
         }
     }
+
 }
